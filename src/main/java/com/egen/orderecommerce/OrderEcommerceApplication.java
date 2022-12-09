c1fe51d5acb540ef8b6d7aba17aa29efbac393b0
priyanshu.goyal <priyanshu.goyal@ukg.com>
2022-12-09 20:45:40 +0530

    1-commit

---
+++ src/main/java/com/egen/orderecommerce/OrderEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.egen.orderecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egen/orderecommerce/OrderEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egen.orderecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
